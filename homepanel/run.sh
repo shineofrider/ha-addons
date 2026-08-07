2eabbb5e5e82bc8173db39142d06efb5802aa146
Skyrider <52279590+shineofrider@users.noreply.github.com>
2026-08-07 19:47:07 +0200

    pdm

--- homepanel/run.sh
+++ homepanel/run.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
+set -e
 
-echo "===== ENV ====="
-env | sort
+echo "[homepanel] starting web interface on port 8099"
 
-echo "===== START ====="
-
-exec uvicorn app:app \
-  --host 0.0.0.0 \
-  --port 8099
+exec uvicorn app:app   --host 0.0.0.0   --port 8099   --proxy-headers   --forwarded-allow-ips "*"
